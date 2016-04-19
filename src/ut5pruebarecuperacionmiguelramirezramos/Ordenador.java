2b8149f137bebfb95c2ed4a0efa622ba61c3ac0c
BattleCat84 <migueli_jrr@hotmail.es>
2016-04-19 20:22:04 +0100

    paso 1 y 2

---
+++ src/ut5pruebarecuperacionmiguelramirezramos/Ordenador.java
@@ -0,0 +1,24 @@
+package ut5pruebarecuperacionmiguelramirezramos;
+
+/**
+ *
+ * @author Migueli Ramos
+ */
+public class Ordenador {
+    
+    //paso 1
+    
+    
+    private String procesador;
+    private String memoria;
+    private String resolucionPantalla;
+    
+    //paso 2
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
