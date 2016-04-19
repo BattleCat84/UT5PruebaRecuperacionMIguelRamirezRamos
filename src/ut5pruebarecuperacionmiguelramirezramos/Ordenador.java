1f5259ddc42be8c4c79b441cedb6959d43e27e2f
BattleCat84 <migueli_jrr@hotmail.es>
2016-04-19 21:28:32 +0100

    paso de documentación en la rama master

--- src/ut5pruebarecuperacionmiguelramirezramos/Ordenador.java
+++ src/ut5pruebarecuperacionmiguelramirezramos/Ordenador.java
@@ -8,6 +8,10 @@ public class Ordenador {
     
     //paso 1
     
+    /**
+     * @since          
+     */
+    
     
     private String procesador;
     private String memoria;
@@ -24,26 +28,45 @@ public class Ordenador {
     }
     
     //paso 3 
+    /**
+     * 
+     * @return 
+     */
     public String getProcesador() {
         return procesador;
     }
-
+    /**
+     * 
+     * @param procesador 
+     */
     public void setProcesador(String procesador) {
         this.procesador = procesador;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public String getMemoria() {
         return memoria;
     }
-
+    /**
+     * 
+     * @param memoria 
+     */
     public void setMemoria(String memoria) {
         this.memoria = memoria;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public String getResolucionPantalla() {
         return resolucionPantalla;
     }
-
+    /**
+     * 
+     * @param resolucionPantalla 
+     */
     public void setResolucionPantalla(String resolucionPantalla) {
         this.resolucionPantalla = resolucionPantalla;
     }
